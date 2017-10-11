e832252ce33fce5b55fca5a28308e85fa60e71bd
Sam Kalum <stkalum@gmail.com>
2017-10-11 17:12:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ function nestedTarget() {
 }
 
 function deepestChild() {
-  let current = document.querySelectorAll("div#grand-node");
+  let current = document.querySelector("div#grand-node");
   let deepest;
 
   while (current) {
     deepest = current;
 
-    current = current.querySelector(current[0]);
+    current = current.querySelector();
   }
 
   return deepest;
