f19da5813831efc9fdaaea9e96a2f6698c008ac8
Sam Kalum <stkalum@gmail.com>
2017-10-11 17:29:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById("nested").querySelector(".target");
+}
+
+function deepestChild() {
+  let current = document.querySelector("div#grand-node");
+  let deepest;
+
+  while (current) {
+    deepest = current;
+
+    current = current.querySelector("*");
+  }
+
+  return deepest;
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll("ul.ranked-list li");
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
